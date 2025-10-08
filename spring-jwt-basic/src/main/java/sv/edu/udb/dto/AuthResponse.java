33b0ea6959a8a106fff10898ffd464a4dfaa80ba
LuisArteaga02 <ca241136@alumno.udb.edu.sv>
2025-10-08 17:31:39 -0600

    Guia 12

--- spring-jwt-basic/src/main/java/sv/edu/udb/dto/AuthResponse.java
+++ spring-jwt-basic/src/main/java/sv/edu/udb/dto/AuthResponse.java
@@ -1,19 +1,14 @@
 package sv.edu.udb.dto;
-
 public class AuthResponse {
-
     private String token;
-
     // Constructor
-    public AuthResponse(String token) {
+    public AuthResponse(String token, String refreshToken) {
         this.token = token;
     }
-
     // Getter y Setter
     public String getToken() {
         return token;
     }
-
     public void setToken(String token) {
         this.token = token;
     }
--- spring-jwt-basic/src/main/java/sv/edu/udb/service/JwtAuthenticationFilter.java
+++ spring-jwt-basic/src/main/java/sv/edu/udb/service/JwtAuthenticationFilter.java
@@ -1,29 +1,21 @@
 package sv.edu.udb.service;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-import org.springframework.stereotype.Component;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
-
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
-
-    public JwtAuthenticationFilter(JwtService jwtService, UserDetailsService userDetailsService) {
-        this.jwtService = jwtService;
-        this.userDetailsService = userDetailsService;
-    }
-
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
@@ -31,18 +23,15 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             FilterChain filterChain
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
-
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
-
         final String jwt = authHeader.substring(7);
         final String username = jwtService.extractUsername(jwt);
-
-        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() ==
+                null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(
@@ -50,11 +39,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                                 null,
                                 userDetails.getAuthorities()
                         );
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                authToken.setDetails(new
+                        WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
-
         filterChain.doFilter(request, response);
     }
 }
